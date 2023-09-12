ec4c488c8dce2728110bb3374f9921dd0d521872
Carlos Delgado <cardel87@gmail.com>
2023-09-12 09:56:34 -0500

    fix tests

--- app/src/test/scala/taller1/SortListTestKElemento.scala
+++ app/src/test/scala/taller1/SortListTestKElemento.scala
@@ -10,25 +10,25 @@ class SortListTestKElemento extends AnyFunSuiteLike {
   test("testK_elementoCase1") {
     val testListA = List(10,3,9,12,1,4,2)
     val objSortList = new SortList()
-    assert(objSortList.k_elemento(testListA, 3) == 4)
+    assert(objSortList.k_elemento(testListA, 4) == 4)
   }
 
   test ("testK_elementoCase2") {
     val testListA = List(10,3,9,12,1,4,2,-1,-3,-5,-7,-9)
     val objSortList = new SortList()
-    assert(objSortList.k_elemento(testListA, 0) == -9)
+    assert(objSortList.k_elemento(testListA, 1) == -9)
   }
 
   test ("testK_elementoCase3") {
     val testListA = List(12,3,9,10,1,4,2,-3)
     val objSortList = new SortList()
-    assert(objSortList.k_elemento(testListA, 6) == 10)
+    assert(objSortList.k_elemento(testListA, 7) == 10)
   }
 
   test ("testK_elementoCase4") {
     val testListA = List(43,12,3,9,10,1,4,2,-3,-5,-7,-9)
     val objSortList = new SortList()
-    assert(objSortList.k_elemento(testListA, 2) == -5)
+    assert(objSortList.k_elemento(testListA, 3) == -5)
   }
 
 
